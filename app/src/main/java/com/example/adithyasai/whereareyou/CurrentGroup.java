53b737a105c46db3ec39ab550f4b815e9ef35b24
adithyasai <anagar17@asu.edu>
2017-04-28 22:04:36 -0700

    added map

--- app/src/main/java/com/example/adithyasai/whereareyou/CurrentGroup.java
+++ app/src/main/java/com/example/adithyasai/whereareyou/CurrentGroup.java
@@ -3,24 +3,51 @@ package com.example.adithyasai.whereareyou;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * Created by adithyasai on 4/8/17.
  */
 
-public class CurrentGroup extends Fragment {
+public class CurrentGroup extends Fragment implements OnMapReadyCallback{
+
+    private GoogleMap gMap;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_current_group, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_current_group, container, false);
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        SupportMapFragment fragment = new SupportMapFragment();
+        transaction.add(R.id.mapView, fragment);
+        transaction.commit();
+        fragment.getMapAsync(this);
+        return view;    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Current Group");
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        gMap=googleMap;
+        setUpMap();
+    }
+
+    public void setUpMap(){
+        gMap.setMyLocationEnabled(true);
+    }
+
+
 }
